33ffbba0cac114af35cb9c5991b92d56354c5a46
Aniol Moreno Batlle <aniolmoreno@gmail.com>
2024-10-03 15:20:28 +0200

    Canviar a Maven

---
+++ src/main/java/com/projecte_amoreno/App.java
@@ -0,0 +1,13 @@
+package com.projecte_amoreno;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
